9b4eeacf53e8fbf5bb2ffdaed3adca0f3e914beb
Nazif <nzfishrak60@gmail.com>
2022-06-07 01:57:29 +0600

    v2

--- thread/Main.java
+++ thread/Main.java
@@ -4,30 +4,33 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
+    static CustomThread thread1 = new CustomThread(
+            "c1",
+            0,
+            500,
+            true,
+            1
+    );
+    static CustomThread thread2 = new CustomThread(
+            "c2",
+            0,
+            1000,
+            true,
+            2
+    );
+    static CustomThread thread3 = new CustomThread(
+            "c3",
+            0,
+            2000,
+            true,
+            10
+    );
     public static void main(String[] args) {
 
+
         Scanner scanner = new Scanner(System.in);
-        CustomThread thread1 = new CustomThread(
-                "c1",
-                0,
-                500,
-                true,
-                1
-        );
-        CustomThread thread2 = new CustomThread(
-                "c2",
-                10,
-                1000,
-                true,
-                2
-        );
-        CustomThread thread3 = new CustomThread(
-                "c3",
-                0,
-                2000,
-                true,
-                10
-        );
+
+
 
         thread1.start();thread2.start();thread3.start();
         System.out.println("Please type the command");
@@ -35,12 +38,16 @@ public class Main {
         while(!commandstr.equalsIgnoreCase("exit")) {
             if (commandstr.equals("1")){
                 System.out.println("Counter in thread1 : "+ thread1.getCounter());
+                isAlive();
+
             }
             else if (commandstr.equals("2")) {
                 System.out.println("Counter in thread2 : "+ thread2.getCounter());
+                isAlive();
             }
             else if (commandstr.equals("3")) {
                 System.out.println("Counter in thread3 : "+ thread3.getCounter());
+                isAlive();
             }
             else if (commandstr.equalsIgnoreCase("1s")) {
                 thread1.stopThread();
@@ -60,6 +67,11 @@ public class Main {
         }
 
 
+    }
+    public static void isAlive() {
+        System.out.println("Thread 1 is alive: " + thread1.isAlive());
+        System.out.println("Thread 2 is alive: " + thread2.isAlive());
+        System.out.println("Thread 3 is alive: " + thread3.isAlive());
     }
 
 }
